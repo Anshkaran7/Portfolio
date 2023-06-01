683944eab3649c472770dec786bea6bee8969b13
Karan <karanchandravanshi217@gmail.com>
2023-06-02 02:04:42 +0530

    Navbar Completed

---
+++ src/Components/Colourful.tsx
@@ -0,0 +1,9 @@
+export const Colorful =() => {
+  return (
+      <div className="z-[-100]">
+        <div className="spot-1 fixed top-[20%] left-[10%] w-32 h-32 rounded-full"></div>
+        <div className="spot-2 fixed top-[40%] right-[10%] w-48 h-48 rounded-full"></div>
+        <div className="spot-3 fixed bottom-[20%] left-[40%] w-32 h-32 rounded-full"></div>
+      </div>
+  );
+};
---
+++ src/Components/Navbar/links.tsx
@@ -0,0 +1,14 @@
+export const navLinks = [
+    {
+    title: "About",
+    url: "About"
+    },
+    {
+    title: "Work",
+    url: "Work"
+    },
+    {
+    title: "Contact",
+    url: "Contact"
+    },
+]
